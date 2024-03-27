b0243c4b7988d2df8881d8700d6d1db425175769
Bee Bayk <beebayk@2400-1A00-B030.ip6.wlink.com.np>
2024-03-27 21:48:09 +0545

    feat: set student if authorized user is student

--- exam/views.py
+++ exam/views.py
@@ -5,6 +5,7 @@ from django.views.generic import TemplateView
 
 
 from exam.models import Exam, ExamAttempt
+from users.models import Student
 
 
 def index_view(request):
@@ -25,15 +26,20 @@ class ExamDetailView(DetailView):
     def post(self, request, *args, **kwargs):
         exam = self.get_object()
         data = deepcopy(request.POST)
+        import ipdb; ipdb.set_trace()
         data.pop("csrfmiddlewaretoken")
-        # import ipdb; ipdb.set_trace()
-        if not request.user.id:
-            student_id = None
+        filtered_data = {}
+        for obj in exam.questions.all():
+            filtered_data[obj.id] = None
+        for k, v in data.items():
+            filtered_data[int(k)] = int(v)
+        if request.user.student:
+            student_id = request.user.id
         else:
             student_id = request.user.id
         exam_attempt = ExamAttempt.objects.create(
-            # student_id=student_id,
+            student_id=student_id,
             exam=exam,
-            exam_data = data
+            exam_data = filtered_data
         )
         return HttpResponse("Success")
