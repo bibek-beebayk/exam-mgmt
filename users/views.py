7940c238c7bae11eb0a65f6c9ab7ad12310dc70f
Bibek Gautam <beebayk0001@gmail.com>
2024-05-26 15:53:18 +0545

    feat: pagination in exam history and other updates

--- users/views.py
+++ users/views.py
@@ -34,8 +34,22 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def profile_view(request):
+    from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
     student = request.user.student
     context = {}
     recent_exams = student.exam_attempts.order_by("-timestamp")
-    context["recent_exams"] = recent_exams
+    page_number = request.GET.get("page")
+    paginator = Paginator(recent_exams, 20)
+    try:
+        page_obj = paginator.get_page(page_number)
+    except PageNotAnInteger:
+        page_obj = paginator.page(1)
+    except EmptyPage:
+        page_obj = paginator.page(paginator.num_pages)
+        
+    # context["recent_exams"] = page_obj.object_list
+    # context["paginator"] = page_obj.paginator
+    context["page_obj"] = page_obj
+    # import ipdb; ipdb.set_trace()
+
     return render(request, "user_profile.html", context)
